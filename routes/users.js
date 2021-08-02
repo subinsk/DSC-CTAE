018b7636d3756c50766bff94a3479c5516c88391
Subin <subinsk284@gmail.com>
2021-08-02 14:06:44 +0530

    done

--- routes/users.js
+++ routes/users.js
@@ -113,67 +113,6 @@ router.get('/logout',(req,res)=>{
     req.logout();
     req.flash('success_msg','You are successfully logged out');
     res.redirect('/login');
-<<<<<<< HEAD
-})
-router.get('/forgot', function(req, res) {
-    res.render('forgot', {
-      user: req.user
-    });
 });
-
-router.post('/forgot', function(req, res, next) {
-    async.waterfall([
-      function(done) {
-        crypto.randomBytes(20, function(err, buf) {
-          var token = buf.toString('hex');
-          done(err, token);
-        });
-      },
-      function(token, done) {
-        User.findOne({ email: req.body.email }, function(err, user) {
-          if (!user) {
-            req.flash('error', 'No account with that email address exists.');
-            return res.redirect('/forgot');
-          }
-  
-          user.resetPasswordToken = token;
-          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-  
-          user.save(function(err) {
-            done(err, token, user);
-          });
-        });
-      },
-      function(token, user, done) {
-        var smtpTransport = nodemailer.createTransport('SMTP', {
-          service: 'SendGrid',
-          auth: {
-            user: '!!! YOUR SENDGRID USERNAME !!!',
-            pass: '!!! YOUR SENDGRID PASSWORD !!!'
-          }
-        });
-        var mailOptions = {
-          to: user.email,
-          from: 'dscctae@gmail.com',
-          subject: 'DSC Password Reset',
-          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-          req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-          done(err, 'done');
-        });
-      }
-    ], function(err) {
-      if (err) return next(err);
-      res.redirect('/forgot');
-    });
-  });
-
-=======
-});
->>>>>>> d7e447eb46f66731dd65d16011b1c3bdb33856ed
 
 module.exports=router;
